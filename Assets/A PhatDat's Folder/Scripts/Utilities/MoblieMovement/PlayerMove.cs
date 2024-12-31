79f57ebce7909157717336a3f8fc0e6345b2e3fd
NHD2507 <huudat25072003@gmail.com>
2024-12-31 11:00:05 +0700

    Init player movement mobile
    tạo player mobile (di chuyển và xoay màn hình)

---
+++ Assets/A PhatDat's Folder/Scripts/Utilities/MoblieMovement/PlayerMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    public float SpeedMove = 5f;
+    private CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Move = transform.right * joystick.Horizontal + transform.forward * joystick.Vertical;
+        controller.Move(Move * SpeedMove * Time.deltaTime);
+    }
+}
